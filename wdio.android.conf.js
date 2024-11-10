d1a8062073f2aec2fe80c754d79c71e6d1b87707
SET <32858918+zd6515843@users.noreply.github.com>
2024-11-10 09:14:18 +0800

    Create wdio.android.conf.js

---
+++ wdio.android.conf.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const { config } = require('./wdio.shared.conf');
+
+// Runner Configuration
+config.port = 4723;
+
+// Specs
+config.specs = [
+  '.test/specs/android/add-note-screen*.js'
+];
+
+// Capabilities
+config.capabilities = [
+  {
+    "appium:platformName": 'Android',
+    "appium:platformVersion": '12.0',
+    "appium:deviceName": 'Pixel5',
+    "appium:automationName": 'UiAutomator2',
+    "appium:app": path.join(process.cwd(), "app/android/ColorNote+Notepad.apk"),
+    "appium:autoGrantPermissions": true
+  }
+]
+
+exports.config = config;
