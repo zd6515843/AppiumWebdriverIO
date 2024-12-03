95cfcbccc038d2408df0fc60f0b14ebf8b8e88a3
SET <32858918+zd6515843@users.noreply.github.com>
2024-12-03 22:56:56 +0800

    Update wdio.android.conf.js

--- config/wdio.android.conf.js
+++ config/wdio.android.conf.js
@@ -13,12 +13,18 @@ config.specs = [
 config.capabilities = [
   {
     "appium:platformName": 'Android',
-    "appium:platformVersion": '12.0',
-    "appium:deviceName": 'Pixel5',
+    "appium:platformVersion": '13.0',
+    "appium:deviceName": 'Pixel 6',
     "appium:automationName": 'UiAutomator2',
     "appium:app": path.join(process.cwd(), "app/android/ColorNote+Notepad.apk"),
     "appium:autoGrantPermissions": true
   }
-]
+];
+
+config.services = [['appium', {
+  args: {
+    relaxedSecurity: true
+  }
+}]];
 
 exports.config = config;
